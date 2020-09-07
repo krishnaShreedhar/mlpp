70308867b9928fb43fa2b65e1501d16d3a5e3ad5
krishna.shreedhar <krishna.shreedhar@gmail.com>
2020-09-07 19:36:59 +0530

    Add more links

--- src/auto_keras_usage/general_approach_to_ml_problems.py
+++ src/auto_keras_usage/general_approach_to_ml_problems.py
@@ -1,5 +1,6 @@
 """
 https://medium.com/kaggle-blog
 http://blog.kaggle.com/2016/07/21/approaching-almost-any-machine-learning-problem-abhishek-thakur/
-
+https://www.linkedin.com/pulse/approaching-almost-any-machine-learning-problem-abhishek-thakur/
+AutoComplete - https://arxiv.org/abs/1507.02188
 """
